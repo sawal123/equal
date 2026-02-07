44ba220a06bc78d48016a9557b5dcc657f86a015
sawal123 <sawalinto@gmail.com>
2026-02-07 13:48:29 +0700

    Feat: link sosmed

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,36 +4,36 @@
           <div class="row">
               <div class="col-lg-5">
                   <div class="subscribe-area">
-                      <img src="{{ asset('storage/'. $logo->logo_1) }}" alt="">
+                      <img src="{{ asset('storage/' . $logo->logo_1) }}" alt="">
                   </div>
               </div>
               <div class="col-lg col-md-6 col-12">
                   <div class="single-footer-items">
                       <h3 class="mb-3">Equallfresh</h3>
                       @php
-                      $wa = $contact->whatsapp;
+                          $wa = $contact->whatsapp;
 
-                      // hapus spasi & strip
-                      $wa = preg_replace('/[^0-9]/', '', $wa);
+                          // hapus spasi & strip
+                          $wa = preg_replace('/[^0-9]/', '', $wa);
 
-                      // jika diawali 08, ganti jadi 628
-                      if (str_starts_with($wa, '08')) {
-                      $wa = '628' . substr($wa, 2);
-                      }
+                          // jika diawali 08, ganti jadi 628
+                          if (str_starts_with($wa, '08')) {
+                              $wa = '628' . substr($wa, 2);
+                          }
                       @endphp
                       <ul class="menu-list list-unstyled">
-                          <li class="mb-2">{{$contact->perusahaan}}</li>
+                          <li class="mb-2">{{ $contact->perusahaan }}</li>
 
                           <li class="mb-2">
-                              {{$contact->alamat}}
+                              {{ $contact->alamat }}
                           </li>
 
                           <li class="mb-2">
-                              <a href="https://wa.me/{{$wa}}" target="_blank">{{$contact->whatsapp}}</a>
+                              <a href="https://wa.me/{{ $wa }}" target="_blank">{{ $contact->whatsapp }}</a>
                           </li>
 
                           <li>
-                              <a href="mailto:{{$contact->email}}">{{$contact->email}}</a>
+                              <a href="mailto:{{ $contact->email }}">{{ $contact->email }}</a>
                           </li>
                       </ul>
                   </div>
@@ -67,9 +67,9 @@
                       <ul class="social-icons">
 
                           @foreach ($socials as $item)
-                          <li>
-                              <a href="#"><i class="{{$item->icon}}"></i></a>
-                          </li>
+                              <li>
+                                  <a href="{{ $item->icon }}"><i class="{{ $item->icon }}"></i></a>
+                              </li>
                           @endforeach
 
                       </ul>
@@ -78,12 +78,12 @@
           </div>
 
           <div class="space40"></div>
-          
+
           <div class="copyright-area">
               <div class="row align-items-center">
                   <div class="col-md-5">
                       <div class="logo">
-                          <a href="#"><img src="{{ asset('storage/'. $logo->logo_2) }}" alt="" /></a>
+                          <a href="#"><img src="{{ asset('storage/' . $logo->logo_2) }}" alt="" /></a>
                       </div>
                   </div>
                   <div class="col-md-7">
